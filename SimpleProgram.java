c94ba80c4f1d1ae110c4de62e5fe2904ce1b908f
Kyehan Popal <kyehanpopal@Kyehans-MacBook-Pro.local>
2025-12-05 20:01:10 -0800

    Initial commit

---
+++ SimpleProgram.java
@@ -0,0 +1,8 @@
+// SimpleProgram.java
+// A minimal Java program that prints "Hello, World!" to stdout.
+
+public class SimpleProgram {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
